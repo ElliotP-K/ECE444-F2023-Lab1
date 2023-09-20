3f14f45e989ab6628b8c77fd4be18cf986a210f8
ElliotP-K <mskrebs2011@hotmail.ca>
2023-09-20 14:00:30 -0400

    Activity 4, commit 2 functional

--- hello.py
+++ hello.py
@@ -4,10 +4,11 @@ from datetime import datetime
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -21,13 +22,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html',
-            form = form, name = session.get('name'))
-
+    return render_template('index.html',form = form, name = session.get('name'), email = session.get('email'))
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
